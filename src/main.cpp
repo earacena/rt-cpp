43bbe517875ea53d21939b54878a1552fd3fee1f
earacena <30157858+earacena@users.noreply.github.com>
2020-10-29 11:13:18 -0400

    Initialized project repository.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*
+ *  Project: rt-cpp
+ *  Author: earacena
+ *  Description: Simple ray-tracer written in C++.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello." << std::endl;    
+    return 0;
+}
