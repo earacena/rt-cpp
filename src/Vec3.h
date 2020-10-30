9c6ac75c564f1dbcd4606e1cae33a4a16c9a8469
earacena <30157858+earacena@users.noreply.github.com>
2020-10-29 20:09:11 -0400

    Added Vec3 class for vector operations, utility functions, and useful type aliases. Added a color function that converts 0.0-1.0 color value to a 0-255 value and prints it. Added simple gradient that fades in red from left to right, green from top to bottom, and blue from top left corner to bottom right.

---
+++ src/Vec3.h
@@ -0,0 +1,155 @@
+/*
+ *  Project: rt-cpp
+ *  Author: earacena
+ *  Filename: Vec3.h
+ *  Description: Simple ray-tracer written in C++.
+ */
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <array>
+#include <cmath>
+
+class Vec3 {
+public:
+    Vec3() : e{0.0, 0.0, 0.0} {}
+    Vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+
+    // Access operators
+    double x() const {
+        return std::get<0>(e);
+    }
+
+    double y() const {
+        return std::get<1>(e);
+    }
+
+    double z() const {
+        return std::get<2>(e);
+    }
+    
+    double operator[](int i) const {
+        return e.at(i);
+    }
+
+    double& operator[](int i) {
+        return e.at(i);
+    }
+
+    // Vector operations
+    Vec3 operator-() const {
+        return Vec3(-1 * std::get<0>(e), 
+                    -1 * std::get<1>(e), 
+                    -1 * std::get<2>(e) );
+    }
+    
+    Vec3 & operator+=(const Vec3 & v) {
+        std::get<0>(e) += std::get<0>(v.e);
+        std::get<1>(e) += std::get<1>(v.e);
+        std::get<2>(e) += std::get<2>(v.e);
+
+        return *this;
+    }
+
+    Vec3 & operator*=(const double t) {
+        std::get<0>(e) *= t;
+        std::get<1>(e) *= t;
+        std::get<2>(e) *= t;
+
+        return *this;
+    }
+
+    Vec3 & operator/=(const double t) {
+        std::get<0>(e) *= (1/t);
+        std::get<1>(e) *= (1/t);
+        std::get<2>(e) *= (1/t);
+
+        return *this;
+    }
+
+    double length_squared() const {
+        return (std::get<0>(e) * std::get<0>(e)) + 
+               (std::get<1>(e) * std::get<1>(e)) + 
+               (std::get<2>(e) * std::get<2>(e));
+    }
+
+    double length() const {
+        return std::sqrt(length_squared());
+    }
+
+    // Data members
+    std::array<double, 3> e;
+};
+
+// Inline utility functions/operators for vector operations
+inline std::ostream & operator << (std::ostream & out, const Vec3 & v) {
+    return out << std::get<0>(v.e) << " " 
+               << std::get<1>(v.e) << " " 
+               << std::get<2>(v.e);
+}
+
+inline Vec3 operator+(const Vec3 & u, const Vec3 & v) {
+    return Vec3( std::get<0>(u.e) + std::get<0>(v.e),
+                 std::get<1>(u.e) + std::get<1>(v.e),
+                 std::get<2>(u.e) + std::get<2>(v.e));
+}
+
+inline Vec3 operator-(const Vec3 & u, const Vec3 & v) {
+    return Vec3( std::get<0>(u.e) - std::get<0>(v.e),
+                 std::get<1>(u.e) - std::get<1>(v.e),
+                 std::get<2>(u.e) - std::get<2>(v.e));
+}
+
+inline Vec3 operator*(const double t, const Vec3 & v) {
+    // Scalar multiplication
+    return Vec3( t * std::get<0>(v.e),
+                 t * std::get<1>(v.e),
+                 t * std::get<2>(v.e));
+}
+
+inline Vec3 operator*(const Vec3 & v, const double t) {
+    // Scalar multiplication
+    return Vec3( std::get<0>(v.e) * t, 
+                 std::get<1>(v.e) * t, 
+                 std::get<2>(v.e) * t );
+}
+
+inline Vec3 operator*(const Vec3 & u, const Vec3 & v) {
+    return Vec3( std::get<0>(u.e) * std::get<0>(v.e),
+                 std::get<1>(u.e) * std::get<1>(v.e),
+                 std::get<2>(u.e) * std::get<2>(v.e));
+}
+
+inline Vec3 operator/(const Vec3 & v, const double t) {
+    return Vec3( std::get<0>(v.e) * (1/t),
+                 std::get<1>(v.e) * (1/t),
+                 std::get<2>(v.e) * (1/t) );
+}
+
+inline double dot(const Vec3 & u, const Vec3 & v) {
+    return (std::get<0>(u.e) * std::get<0>(v.e)) +
+           (std::get<1>(u.e) * std::get<1>(v.e)) +
+           (std::get<2>(u.e) * std::get<2>(v.e));
+}
+
+inline Vec3 cross(const Vec3 & u, const Vec3 & v) {
+    Vec3 p = Vec3( (std::get<1>(u.e) * std::get<2>(v.e)) - 
+                    (std::get<2>(u.e) * std::get<1>(v.e)),
+                   (std::get<2>(u.e)* std::get<0>(v.e)) -
+                    (std::get<0>(u.e) * std::get<2>(v.e)),
+                   (std::get<0>(u.e) * std::get<1>(v.e)) - 
+                    (std::get<1>(u.e) * std::get<0>(v.e)) );
+
+    return p;
+}
+
+inline Vec3 unit_vector(const Vec3 & v) {
+    return v / v.length();
+}
+
+// Type aliases
+using ColorRGB = Vec3;
+using Point3 = Vec3;
+
+#endif // VEC3_H
--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,22 @@
 #include <string>
 #include <cstdlib>
 
+#include "Vec3.h"
+
 void print_usage() {
     std::cerr << "Usage:" << std::endl 
               << "\t./rt [width] [height]" << std::endl 
               << "\tArgs:" << std::endl
               << "\twidth: numeric value, 1-2000" << std::endl
               << "\theight: numeric value, 1-2000" << std::endl;
+}
+
+void write_color(std::ostream & output, const ColorRGB color) {
+    // RGB triplets follow arithmentic notation from 0-1,
+    // convert to a color value from 0-255 and output
+    output << static_cast<int>(255.999 * color.x()) << " "
+           << static_cast<int>(255.999 * color.y()) << " "
+           << static_cast<int>(255.999 * color.z()) << std::endl;
 }
 
 void render(const uint16_t image_width, const uint16_t image_height) {
@@ -32,10 +42,21 @@ void render(const uint16_t image_width, const uint16_t image_height) {
     // every line is a RGB triplet
     // col is image_width pixels
     // row is image_height pixels
-    // Iterate every pixel
-    for (int row = image_height; row > 0; --row) {
+    // Iterate every pixel, slowly fading in green values left to right,
+    // and fading out red values top to bottom
+    double r = 0.0;
+    double g = 0.0;
+    double b = 0.0;
+    ColorRGB color = ColorRGB(0.0, 0.0, 0.0);
+    for (int row = image_height-1; row >= 0; --row) {
         for(int col = 0; col < image_width; ++col) {
-            std::cout << "100 100 10" << std::endl;
+            r = static_cast<double>(col) / static_cast<double>(image_width-1);
+            g = static_cast<double>(row) / static_cast<double>(image_height-1);
+            b = static_cast<double>(row+col) / static_cast<double>(image_width+image_height-2);
+            std::get<0>(color.e) = r;
+            std::get<1>(color.e) = g;
+            std::get<2>(color.e) = b;
+            write_color(std::cout, color);
         }
     }
 
