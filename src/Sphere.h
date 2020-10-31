4b5fbc7798c8d1543a53753d6208f0674f9bb9e3
earacena <30157858+earacena@users.noreply.github.com>
2020-10-31 16:20:03 -0400

    Simplified sphere intersection formula.

--- src/Sphere.h
+++ src/Sphere.h
@@ -15,15 +15,20 @@ double hit_sphere(const Point3 & center, double radius, const Ray & ray) {
     // Sphere equation: (p - c) dot (p - c) = r^2
     // Intersection:
     //      ((o + td) - c) dot ((o + td) - c) = r^2 
-    //      t^2(d dot d) +  2(o - c)td + (o - c) dot (o - c) - r^2 = 0
+    //      t^2(d dot d) +  2(o - c) dot td + (o - c) dot (o - c) - r^2 = 0
     //            a              b                    c
-
-    // Quadratic formula, using sphere intersection formula
+    // Given that: 
+    // b = 2 ((o - c) dot d)
+    // b = 2h
+    // (-b +- sqrt(b^2 - 4ac))/2a, Quadratic formula
+    // becomes
+    // (-h +- sqrt(h^2 - ac))/a, simplified formula
+    
     Vec3 diff_o_c = ray.origin() - center;
     double a = dot(ray.direction(), ray.direction());
-    double b = 2.0 * dot(diff_o_c, ray.direction());
+    double h = dot(diff_o_c, ray.direction());
     double c = dot(diff_o_c, diff_o_c) - (radius * radius);
-    double discriminant = (b*b) - (4*a*c);
+    double discriminant = (h*h) - (a*c);
 
     // d < 0, no intersection
     // d = 0, 1 intersection, surface
@@ -34,7 +39,8 @@ double hit_sphere(const Point3 & center, double radius, const Ray & ray) {
         return -1.0;
     } else {
         // if intersections, give t-value >= 0
-        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+        // using simplified formula
+        return (-h - std::sqrt(discriminant)) / a;
     }
 }
 
