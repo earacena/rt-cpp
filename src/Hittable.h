a84dd0af2fb12d63ae9c9b25a015f215ce095e21
earacena <30157858+earacena@users.noreply.github.com>
2020-10-31 23:13:41 -0400

    Added front face tracking to HitRecord that will ensure all surface normals point against a ray. Updated Sphere hit method to reflect front face tracking.

--- src/Hittable.h
+++ src/Hittable.h
@@ -8,10 +8,20 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+#include "Ray.h"
+
 struct HitRecord {
     Point3 point;
     Vec3 normal;
     double t;
+    bool front_face;
+
+    inline void set_face_normal(const Ray & ray, const Vec3 & outward_normal) {
+        // If dot product is positive, ray is inside otherwise outside
+        // Always have normals point against ray
+        front_face = (dot(ray.direction(), outward_normal) < 0.0);
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class Hittable {
